0857ba9852fb2a34acb493a889fbfbbb41a931ef
Sangenido <139924832+Sangenido@users.noreply.github.com>
2023-07-19 15:48:18 -0300

    Add files via upload

---
+++ Aula_4_teste3.py
@@ -0,0 +1,6 @@
+aluno = input('Digite o nome do aluno: ')
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1+n2)/2
+print('A média do aluno é {}'.format(media))
+print('O nome do aluno é : ' + aluno)
---
+++ Aula_4_teste6.py
@@ -0,0 +1,2 @@
+vlr=float(input('Digite um valor em real, R$ '))
+print('O valor em R${},em dólar é U$ {}'.format(vlr,vlr/4.8))
